b8070559c39e84d83925954c8efb4233ff74aa6a
Steven S <stevenjslyker@gmail.com>
2019-03-31 20:55:19 -0600

    finalish

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double-Double');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Famous Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Boca Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('4x4');
